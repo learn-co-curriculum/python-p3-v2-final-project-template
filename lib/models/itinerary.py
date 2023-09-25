d770639f585b47ddc663a3bfbe01aad34888aca6
Spencer <eklund.spencer@outlook.com>
2023-09-25 11:35:53 -0600

    added properties to trip

--- lib/models/itinerary.py
+++ lib/models/itinerary.py
@@ -2,10 +2,47 @@
 from models.__init__ import CURSOR, CONN
 
 class Trip:
-    def __init__(self, day, trip, id=None):
+    def __init__(self, day, trip, activity, id=None,):
         self.day = day
         self.trip = trip
         self.id = id
+        self.activity = activity
+
+    @property
+    def day(self):
+        return self._day
+    
+    @day.setter
+    def day(self, day):
+        if isinstance(day, str):
+            self._day = day
+        else:
+            raise Exception("Day must be a string that is a weekday")
+
+    @property
+    def trip(self):
+        return self._trip
+    
+    @trip.setter
+    def trip (self, trip):
+        if isinstance(trip, str) and len(trip) > 3 and not hasattr(self, "trip"):
+            self._trip = trip
+        else:
+            raise Exception("Trip must be unique string over 3 characters")
+        
+
+    @property
+    def activity(self):
+        return self._activity
+    
+    @activity.setter
+    def activity(self, activity):
+        if isinstance(activity, Activity):
+            self._activity = activity
+        else:
+            raise Exception("Activity must be from the activity class")
+
+
 
 class Activity:
     def __init__(self, activity, description, price):

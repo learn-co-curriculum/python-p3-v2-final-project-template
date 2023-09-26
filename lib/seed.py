529327b6fd0750a190f8615d538e1cfca036dfdd
Jmillamena <jcmillamena@gmail.com>
2023-09-26 12:33:46 -1000

    edits swimming

--- lib/seed.py
+++ lib/seed.py
@@ -11,12 +11,10 @@ def seed_database():
     Activity.drop_table()
     Activity.create_table()
 
-
     summer = Trip.create("Summer Break", "Cancun")
     swimming = Activity.create(
-        "Swimming", "Swimming at the Infinity Pool", 0.00, "Thursday", summer)
-    
-    
+        "Swimming", "Swimming at the Infinity Pool", 0.00, "Thursday", summer.id)
+
 
 seed_database()
 print("Seeded database")

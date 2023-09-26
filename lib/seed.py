65976f423f3ac812959ade47fe814a8c7f65c371
Jmillamena <jcmillamena@gmail.com>
2023-09-26 11:13:34 -1000

    updates seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -2,13 +2,19 @@
 
 from models.__init__ import CONN, CURSOR
 from models.itinerary import Trip
+from models.itinerary import Activity
 
 
 def seed_database():
     Trip.drop_table()
     Trip.create_table()
 
+    Activity.create_table()
+    Activity.drop_table()
+
     summer = Trip.create("Summer Break", "Cancun")
+    swimming = Trip.create(
+        "Swimming", "Swimming at the Infinity Pool", 0.00, "Thursday")
 
 
 seed_database()

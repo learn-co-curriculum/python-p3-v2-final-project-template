92acd411d49256ce1b0ac5b41915670d80fd3e87
eklspe12 <123013923+eklspe12@users.noreply.github.com>
2023-09-26 15:19:32 -0600

    Update seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -8,9 +8,9 @@ from models.itinerary import Activity
 def seed_database():
     Trip.drop_table()
     Trip.create_table()
-
-    Activity.create_table()
     Activity.drop_table()
+    Activity.create_table()
+    
 
     summer = Trip.create("Summer Break", "Cancun")
     swimming = Trip.create(

3732f5fbea32ee77056ec3fa0d39c6a59cbdd0a7
charlesfeatherstone <charlesfstone11@gmail.com>
2023-11-07 16:38:43 -0500

    cli changed

--- lib/cli.py
+++ lib/cli.py
@@ -19,18 +19,10 @@ def main():
             exit_program()
         elif choice == "1":
             Parent.names()
-        elif choice in Parent.parent_names:
-            for a in Parent.all_parents:
-                bio=''
-                if a.name == choice:
-                    bio=a.bio
-                    print(bio)
-        elif choice in Child.name_list:
-            for a in Child.spawn:
-                bio=''
-                if a.name == choice:
-                    bio=a.bio
-                    print(bio)
+        
+        elif choice == "2":
+            sub()
+
         else:
             print("Invalid choice")
 
@@ -39,6 +31,35 @@ def menu():
     print("Please select an option:")
     print("0. Exit the program")
     print("1. Some useful function")
+    print("2. Bio")
+
+def sub():
+    while True:
+        sub_menu()
+        sub_choice = input("> ")
+        if sub_choice in Parent.parent_names:
+            for a in Parent.all_parents:
+                bio=''
+                if a.name == sub_choice:
+                    bio=a.bio
+                    print('[BIO]')
+                    print(bio)
+        elif sub_choice in Child.name_list:
+            for a in Child.spawn:
+                bio=''
+                if a.name == sub_choice:
+                    bio=a.bio
+                    print('[BIO]')
+                    print(bio)
+        elif sub_choice == "back":
+            break
+        else:
+            print('[invalid choice, try again.]')
+
+def sub_menu():
+    print('[Options]')
+    print('Type back to go back')
+    print('Type a God\'s name')
 
 
 if __name__ == "__main__":

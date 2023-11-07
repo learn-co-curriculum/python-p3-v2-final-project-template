b2e12999ca055303f65f18863e91a1b0901b13a2
hayleerees <rae8504679@gmail.com>
2023-11-06 22:57:46 -0500

    tweaked some things, added notes, have a create character now too

--- lib/models/characters.py
+++ lib/models/characters.py
@@ -27,8 +27,11 @@ class Character:
     
     @name.setter
     def name(self, name):
-        if isinstance(name, str) and len(name) > 3:
-            self._name = name
+        if type(name) == str:
+            if len(name) >= 5:
+                self._name = name
+            else: raise ValueError('!!Name must be at least 5 characters!!')
+        else: raise ValueError('!!Name must be a string!!')
 
     @property
     def char_class(self):
@@ -118,6 +121,7 @@ class Character:
     def active(self, active):
         self._active = active
 
+# UPDATES if id is already in the database -- SAVES a new character to the database ans assigns id
     def save(self):
         if self.id:
             sql = """
@@ -136,21 +140,18 @@ class Character:
             id_sql = """
                 SELECT LAST_INSERT_ROWID() FROM characters
             """
-            new_id_tuple = CURSOR.execute(id_sql).fetchoe()
+            new_id_tuple = CURSOR.execute(id_sql).fetchone()
             self.id = new_id_tuple[0]
 
+#creates a new character and saves it to the database
     @classmethod
-    def create_char(cls):
-        """ Create a new table to persist the attributes of Department instances """
-        sql = """
-            CREATE TABLE IF NOT EXISTS departments (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            location TEXT)
-        """
-        CURSOR.execute(sql)
-        CONN.commit()
+    def create_char(cls, name, char_class, race, strengh, dexterity, constitution, intelligence, wisdom, charisma, alignment, player, active):
+        character = Character(name, char_class, race, strengh, dexterity, constitution, intelligence, wisdom, charisma, alignment, player, active)
+        character.save()
+        return character
+        
 
+#deletes a character from the database and gets rid of id
     def delete_char(self):
         sql = """
             DELETE FROM characters WHERE id = ?;
@@ -160,12 +161,14 @@ class Character:
         CONN.commit()
         self.id = None
 
+#given a row from the database---return with new instance with info from database
     @classmethod
     def dnd_data(cls, row_tuple):
         character = Character(row_tuple[1])
         character.id = row_tuple[0]
         return character
 
+#returns a Character from dnd_data and gives instances of all Characters from the database
     @classmethod
     def all(cls):
         sql = """
@@ -175,4 +178,5 @@ class Character:
         return [Character.dnd_data(row) for row in char_list]
     
     
-#create an object, view related objects, and find an object by attribute.
+# view related objects, and find an object by attribute
+#char_class, race, strengh, dexterity, constitution, intelligence, wisdom, charisma, alignment, player, active

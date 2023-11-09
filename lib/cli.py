778c2496968fd2b6dcb6a00763ba89db31c278fb
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-11-08 20:24:46 -0600

    whops

--- lib/cli.py
+++ lib/cli.py
@@ -14,19 +14,49 @@ def main():
         elif choice == "2":
             player_options()
         elif choice == "3":
-            exit_program()
-        elif choice == "4":
             login_menu()
+            # Would be a menu()
+                # while True:
+                    # login_menu()
+                    # Choice == "1":
+                    #     select_player()
+                    # choice == "2":
+                    #     select_world()
+        elif choice == "4":
+            exit_program()
         else:
             print("Please Make A Valid Choice")
 
 def menu():
+    print("""
+            .,-:;//;:=,
+         . :H@@@MM@M#H/.,+%;,
+      ,/X+ +M@@M@MM%=,-%HMMM@X/,
+     -+@MM; $M@@MH+-,;XMMMM@MMMM@+-
+    ;@M@@M- XM@X;. -+XXXXXHHH@M@M#@/.
+  ,%MM@@MH ,@%=            .---=-=:=,.
+  -@#@@@MX .,              -%HX$$%%%+;
+ =-./@M@M$                  .;@MMMM@MM:
+ X@/ -$MM/                    .+MM@@@M$
+,@M@H: :@:                    . -X#@@@@-
+,@@@MMX, .                    /H- ;@M@M=
+.H@@@@M@+,                    %MM+..%#$.
+ /MMMM@MMH/.                  XM@MH; -;
+  /%+%$XHH@$=              , .H@@@@MX,
+   .=--------.           -%H.,@@@@@MX,
+   .%MM@@@HHHXX$$$%+- .:$MMX -M@@MM%.
+     =XMMM@MM@MM#H;,-+HMM@M+ /MMMX=
+       =%@M@M#@$-.=$@MM@@@M; %M%=
+         ,:+$+-,/H#MMMMMMM@- -,
+               =++%%%%+/:-.
+""")
     print("________________________")
     print("Please select an option")
     print("------------------------")
     print("1. World Options")
     print("2. Player Options")
-    print("3. Exit the program")
+    print("3. Login Screen?")
+    print("4. Exit the program")
 
 def world_options():
     while True:
@@ -53,6 +83,7 @@ def world_options():
             print("Please Make A Valid Choice")
         
 def login_menu():
+    print("Theres Nothing Here Right Now")
     print(f"World Selected:{selected_world}")
     print(f"Player Selected:{selected_player}")
 
@@ -86,23 +117,21 @@ def player_options():
             name = input('Find your character by their name: ')
             player = Player.find_by_name(name)
             print(player)
-        elif choice == "5":
-            break
-        elif choice == '6':
+        elif choice == '5':
             id = input('Please input a id')
             name= input('Please input a name')
             player = Player.find_by_name(name)
             world_instance = World.find_by_id(id)
             player.login(world_instance)
             player.worlds()
-        elif choice == '7':
+        elif choice == '6':
             name = input('please input player name ')
             player = Player.find_by_name(name)
             print(player.worlds())   
+        elif choice == "7":
+            break
         else:
             print("Please Make A Valid Choice")
-        
-        
 
 
 def create_player_menu():
@@ -145,7 +174,9 @@ def player_menu():
     print("2. Display Player's")
     print("3. Remove Player")
     print('4. Find Player by name')
-    print("5. Back to Main Menu")
+    print("5. Login A player to a world")
+    print("6. Search a Player Worlds")
+    print("7. Back to Main Menu")
 
 def select_world():
     while True:

32918a6fe737a9ea5a6da1ee6e954f918fb4cedd
Austin Byrd <byrda05@gmail.com>
2023-11-23 12:54:39 -0600

    click/database skele

--- lib/time_clock_app/__init__.py
+++ lib/time_clock_app/__init__.py
@@ -2,3 +2,28 @@ import sqlite3
 
 CONN = sqlite3.connect('company.db')
 CURSOR = CONN.cursor()
+
+import click
+
+@click.group()
+def cli():
+    """Time Clocking Application"""
+    pass
+
+@click.command()
+def clock_in():
+    """Clock in for work"""
+    # Implementation here
+    pass
+
+@click.command()
+def clock_out():
+    """Clock out of work"""
+    # Implementation here
+    pass
+
+cli.add_command(clock_in)
+cli.add_command(clock_out)
+
+if __name__ == "__main__":
+    cli()

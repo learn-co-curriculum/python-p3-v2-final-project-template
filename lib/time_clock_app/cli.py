35ea15b405790fb9c387f246919f78d6ddd93b54
matthew <shaggysci3@gmail.com>
2023-11-28 06:32:11 -0500

    put in cli

--- lib/time_clock_app/cli.py
+++ lib/time_clock_app/cli.py
@@ -1,31 +1,17 @@
-#user class
-# can't figure out how I used ipdb in the code challenge XD gotta do tat ad make this functional to set pinput values to an array or value in the usernames and passwords....
-import ipdb
-class User:
-    all = []
 
-    def __init__(self, pass_word, user_name):
-        self._password = pass_word
-        self._username = user_name
-        type(self).all.append(self)
-# props for user
-    @property
-    def password(self):
-        return self._password
-# setters for user
-
-# cli info setting to txt file
 def create_user_name(user_names):
-    with open('./time_clock_app/users.txt', 'a') as un:
+    with open('./users.txt', 'a') as un:
         for user in user_names:
+            # add '\n' to write grades on separate lines
             un.write(user + '\n')
-# cli for project
+
 if __name__ == '__main__':
     user_names = []
 
     user = input("unsername, password: ")
     while user:
         user_names.append(user)
+        # end when no grade is entered
         user = input("username, password: ")
 
     create_user_name(user_names)

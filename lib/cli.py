0ae40a9d01a41a177a4d5fe9d57b460797dd9195
matthew <shaggysci3@gmail.com>
2023-11-28 08:41:47 -0500

    menu options for user_info

--- lib/cli.py
+++ lib/cli.py
@@ -3,7 +3,8 @@
 from helpers import (
     exit_program,
     create_username,
-    create_password
+    create_password,
+    user_info
 )
 
 
@@ -17,6 +18,8 @@ def main():
             create_username()
         elif choice == "2":
             create_password()
+        elif choice == "3":
+            user_info()
         else:
             print("Invalid choice")
 
@@ -26,6 +29,7 @@ def menu():
     print("0. Exit the program")
     print("1. create username")
     print("2. create password")
+    print("3. user_info menu")
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -6,13 +6,39 @@ def create_username():
     user = input("enter unsername: ")
     username = Username(user)
     print (f'your username is: {username}')
+    return username
     
 def create_password():
     password = input("enter password: ")
     print(f'your password is: {password}')
 
 def user_info():
-    pass
+    user_menu()
+
+    choice = input("> ")
+    if choice == "0":
+        exit_program
+    elif choice == "1":
+        print("______password/username______")
+        print("")
+    elif choice == "2":
+        print("  ______clock-in times______")
+        print("place holder for clock-out times")
+        print("")
+    elif choice == "3":
+        print("______clock-out times______")
+        print("place holder for clock-out times")
+        print("")
+    else:
+        print("______Invalid choice______")
+        print("")
+
+def user_menu():
+    print("Please select an action:")
+    print("0. exit")
+    print("1. manage username/password")
+    print("2. see clock-in time")
+    print("3. see clock-out time")
 
 def exit_program():
     print("Goodbye!")

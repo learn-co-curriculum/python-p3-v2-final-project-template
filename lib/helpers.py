1ab4501430e73afef230f4a62fec59649db0bb02
matthew <shaggysci3@gmail.com>
2023-11-28 11:16:37 -0500

    adding UI clean at the end of inputs

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,18 +1,22 @@
 # lib/helpers.py
 from time_clock_app.user_info import Username
+import os 
 
 
 def create_username():
+    os.system('cls' if os.name == 'nt' else 'clear')
     user = input("enter unsername: ")
     username = Username(user)
     print (f'your username is: {username}')
     return username
     
 def create_password():
+    os.system('cls' if os.name == 'nt' else 'clear')
     password = input("enter password: ")
     print(f'your password is: {password}')
 
 def user_info():
+    os.system('cls' if os.name == 'nt' else 'clear')
     user_menu()
 
     choice = input("> ")
@@ -34,6 +38,7 @@ def user_info():
         print("")
 
 def user_menu():
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("Please select an action:")
     print("0. exit")
     print("1. manage username/password")
@@ -42,4 +47,6 @@ def user_menu():
 
 def exit_program():
     print("Goodbye!")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
     exit()

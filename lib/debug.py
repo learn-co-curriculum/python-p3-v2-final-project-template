0c413459e510ba862e5971cd886f8a5a9918187b
hadil <hihijazi@gmail.com>
2024-01-14 05:43:06 -0500

    save changes

--- lib/debug.py
+++ lib/debug.py
@@ -33,4 +33,60 @@ print(Boxing.membership_required)
 # L1.save()
 # Boxing.save()
 
+L1 = Location("Chicago")
+L2 = Location("Memphis")   
+L3 = Location("St. Louis")
+L4 = Location("Louisville")
+L5 = Location("Seatle")
+
+t1 = Trainer("Bob", "Thornton") 
+t2 = Trainer("Tony", "Little")
+t3 = Trainer("Tracy", "Anderson")
+t4 = Trainer("Jillian", "Michales")
+t5 = Trainer("Bob", "Harper")
+t6 = Trainer("Kathy", "Smith")
+
+e1 = Exercise("Boxing")  
+e2 = Exercise("Spin Class")
+e3 = Exercise("Zumba")
+e4 = Exercise("Cycling")
+e5 = Exercise("Yoga")   
+
+LocationID = 1
+LocationName = "Chicago"
+TrainerID = 1 
+TrainerFirstName = "Bob"
+TrainerLastName = "Thornton"  
+ExerciseID = 1  
+ExerciseName = Boxing   
+MemberID = 1
+MemberFirstName = "Jeffrey"   
+MemberLastName = "Davis"  
+MemberMembership = "Basic"
+
+LocationID = 2
+LocationName = "Memphis"
+TrainerID = 3
+TrainerFirstName = "Tracy" 
+TrainerLastName = "Anderson" 
+ExerciseID = 2  
+ExerciseName = "Spin Class"   
+MemberID = 3
+MemberFirstName = "Hadil"   
+MemberLastName = "Hijazi"  
+MemberMembership = "Premium"
+
+LocationID = 4
+LocationName = "Louisville"
+TrainerID = 2 
+TrainerFirstName = "Tony" 
+TrainerLastName = "Little" 
+ExerciseID = 1  
+ExerciseName = Boxing   
+MemberID = 2
+MemberFirstName = "Katie"   
+MemberLastName = "Nowicki"  
+MemberMembership = "Premium"
+
+
 ipdb.set_trace()

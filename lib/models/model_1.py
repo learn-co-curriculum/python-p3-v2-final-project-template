23a3675f1a36ad439ded7f1f4274b22a2d8205a0
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-19 14:57:56 -0500

    CREATE README

---
+++ lib/models/model_1.py
@@ -0,0 +1,22 @@
+# class Gym:
+#     location 
+#     trainer 
+#     class type 
+#     member 
+
+# class Membership:
+#     name 
+#     is premium? conditional -- figure that out 
+
+# class program:
+#     name 
+#     date of class 
+#     is premium member 
+
+
+# class Trainer 
+#     name 
+#     program 
+#     location 
+
+    

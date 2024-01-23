8257e8b99697dcb7d465ad8e4db30cf4ba4ee9b6
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-23 15:09:04 -0500

    fix location and member merge

--- lib/models/location.py
+++ lib/models/location.py
@@ -1,9 +1,82 @@
+import sqlite3 
+
+CONN = sqlite3.connect('lib/gym.db')
+CURSOR = CONN.cursor()
+
+
 class Location:
-    def __init__(self, city):
-        self.city = city # Needs to be property
+    def __init__(self, city, id = None):
+        self.city = city
+        self.id = id
 
     def display_info(self):
         print(f"Location: {self.name}")
+
+    @classmethod
+    def create_location_table(cls):
+        query = """
+            CREATE TABLE IS NOT EXISTS `location_table` (
+            id INTEGER PRIMARY KEY,
+            city TEXT);
+        """
+        CURSOR.execute(query)
+        CONN.commit()
+
+    # @classmethod 
+    # def drop_table(cls):
+    #     query = """
+    #         DROP TABLE IF EXISTS `location_table`;
+    #     """
+    #     CURSOR.execute(query)
+    #     CONN.commit()
+
+
+    def save(self):
+        query = """
+            INSERT INTO `location_table` ( `city`)
+            VALUES (?)
+        """
+        CURSOR.execute(query, (self.city))
+        CONN.commit()
+        self.id = CURSOR.lastrowid 
+
+    @classmethod 
+    def create_location_row(cls, city):
+        city = cls(city)
+        city.save()
+        return city 
+    
+    @classmethod 
+    def new_location_db(cls, row):
+        location = cls (
+                id = row[0],
+                city = row[1]
+            )
+        print(location.city)
+        return location 
+    
+    @classmethod 
+    def get_all_locations(cls):
+        sql = """
+            SELECT * FROM location_table
+        """
+        return [cls.new_location_db(one_row) for one_row in CURSOR.execute(sql).fetchall()]
+    
+    @classmethod 
+    def find_by_name(cls, city):
+        sql = """
+            SELECT * FROM location_table
+            WHERE city = ? 
+            LIMIT 1
+        """
+        row = CURSOR.execute(sql, (city)).fetchone()
+        if not row:
+            return None
+        return Location(
+            city = row[1],
+            id = row[0]
+        )
+
 
 # Example locations
 chicago_location = Location("Chicago")
--- lib/models/member.py
+++ lib/models/member.py
@@ -1,5 +1,11 @@
+import sqlite3 
+
+CONN = sqlite3.connect('lib/gym.db')
+CURSOR = CONN.cursor()
+
 class Member:
-    def __init__(self, first_name, last_name, membership_type="Basic"):
+    def __init__(self, id, first_name, last_name, membership_type="Basic"):
+        self.id = id
         self.first_name = first_name # Needs to be property
         self.last_name = last_name
         self.membership_type = membership_type # Needs to be property
@@ -19,6 +25,76 @@ class Member:
         classes_info = f"Classes Attended: {', '.join([exercise.name for exercise in self.classes_attended])}"
 
         print(f"Member Name: {self.name}\n{membership_info}\n{classes_info}")
+
+    @classmethod 
+    def create_member_table(cls):
+        query = """
+            CREATE TABLE IF NOT EXISTS `member_table` (
+            id INTEGER PRIMARY KEY, 
+            first_name TEXT, 
+            last_name TEXT, 
+            membership_type TEXT);
+        """
+        CURSOR.execute(query)
+        CONN.commit()
+
+    # @classmethod 
+    # def drop_table(cls):
+    #     query = """
+    #         DROP TABLE IF EXISTS `member_table`;
+    #     """
+    #     CURSOR.execute(query)
+    #     CONN.commit()
+
+    def save(self):
+        query = """
+            INSERT INTO `member_table` ( `first_name`, `last_name`, `membership_type` )
+            VALUES (?, ?, ?)
+        """
+        CURSOR.execute(query, (self.first_name, self.last_name, self.membership_type))
+        CONN.commit()
+        self.id = CURSOR.lastrowid 
+
+    @classmethod 
+    def create_member_row(cls, first_name, last_name, membership_type):
+        member = cls(first_name, last_name, membership_type)
+        member.save()
+        return member 
+    
+    @classmethod 
+    def new_member_db(cls, row):
+        member = cls (
+                id = row[0],
+                first_name = row[1],
+                last_name = row[2],
+                membership_type = row[3]
+            )
+        print(member.first_name, member.last_name, member.membership_type)
+        return member
+    
+    @classmethod 
+    def get_all_members(cls):
+        sql = """
+            SELECT * FROM member_table
+        """
+        return [cls.new_member_db(one_row) for one_row in CURSOR.execute(sql).fetchall()]
+    
+    @classmethod 
+    def find_by_name(cls, first_name, last_name):
+        sql = """
+            SELECT * FROM member_table
+            WHERE first_name = ? 
+            AND last_name = ?
+            LIMIT 1
+        """
+        row = CURSOR.execute(sql, (first_name, last_name)).fetchone()
+        if not row:
+            return None
+        return Member(
+            first_name = row[1],
+            last_name = row[2],
+            id = row[0]
+        )
 
 # Members
 basic_member = Member("Jeffery")

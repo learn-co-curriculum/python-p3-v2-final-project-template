8084f5ef3642fedf6813ca911d54a8ee6dd3509c
Ikram Chouichi <ikram@netronian.com>
2024-03-04 10:10:36 -0500

    ORM Validation and Properties for band

--- lib/models/band.py
+++ lib/models/band.py
@@ -1,3 +1,6 @@
+from __init__ import CURSOR, CONN
+from utils import custom_property, SQLDropTable
+
 class Band:
     def __init__(self, name, members, genre, home_city):
         self.name = name   
@@ -53,3 +56,63 @@ class Band:
 
     def bands(self):
          return list(set([concert.band for concert in self.concert()]))
+    
+    
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS bands (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            members TEXT,
+            genre TEXT,
+            home_city_id INTEGER,
+            FOREIGN KEY (home_city_id) REFERENCES cities(id))
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS bands
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        members_str = ','.join(self.members)
+        sql = """
+            INSERT INTO bands (name, members, genre, home_city_id)
+            VALUES (?, ?, ?, ?)
+        """
+        CURSOR.execute(sql, (self.name, members_str, self.genre, self.home_city_id))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+        type(self).working_insts[self.id] = self
+
+    @classmethod
+    def create(cls, name, members, genre, home_city_id):
+        band = cls(name, members, genre, home_city_id)
+        band.save()
+        return band
+
+    @classmethod
+    def instance_from_db(cls, row):
+        band_id, name, members_str, genre, home_city_id = row
+        members = members_str.split(',')
+        band = cls.working_insts.get(band_id)
+        if band:
+            band.name = name
+            band.members = members
+            band.genre = genre
+            band.home_city_id = home_city_id
+        else:
+            band = cls(name, members, genre, home_city_id)
+            band.id = band_id
+        return band
+
+Band.create_table()
+Band.create('The Example', ['Member 1', 'Member 2'], 'Rock', 1) 

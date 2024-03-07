c1f29319d47a501443c6ee313ca85585bf771b48
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-03-07 10:45:44 -0500

    name changes

--- lib/models/seed.py
+++ lib/models/seed.py
@@ -22,7 +22,7 @@ def reseed():
     metallica = Band.create("Metallica", ["James Hetfield", "Lars Ulrich", "Cliff Burton", "Jason Newsted"], "Thrash Metal", "Los Angeles")
     glass_animals = Band.create("Glass Animals", ["Dave Bayley", "Drew MacFarlane", "Edmund Irwin-Singer", "Joe Seaward"], "Indie Rock", "Oxford")
     jam = Concert.create("Jam Fest", "9/8/2010", ["DJ Khaled", "Mos Def"], "New York", tickets)
-    ball = Concert.create("The Ball", "4/8/2025", [metallica, 1, "Glass Animals"], "International Waters", tickets2) # 1 will be the id of DJ Khaled
+    ball = Concert.create("Mayor's Ball", "4/8/2025", [metallica, 1, "Glass Animals"], "International Waters", tickets2) # 1 will be the id of DJ Khaled
 
 # import ipdb 
 # ipdb.set_trace()

f852eaa11cedf0207c2fe4b916260f41cc731c70
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-03-21 15:48:13 -0700

    start of reviewer class and table

--- lib/classes/reviewer.py
+++ lib/classes/reviewer.py
@@ -1,4 +1,39 @@
+from __init__ import CURSOR, CONN
+
 class Reviewer:
-    def __init__(self):
-        pass
+    def __init__(self, name, id=None):
+        self.id = id
+        self.name = name
+        self.posts = []
     
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        elif len(name) <= 2:
+            raise ValueError("Name must be at least two chars inclusive.")
+        else:
+            self._name = name
+
+    def __repr__(self):
+        return f"<Reviewer {self.id}: {self.name}>"
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IS NOT EXISTS reviewers (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            task_list TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+    
+    def posts_to_review(self):
+        # return [post for post in self.posts if not post.reviewed]
+        pass

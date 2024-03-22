32391dd88820b7e6804231a811a817fad06758a2
Sung-Jin Ahn <sahn0307@gmail.com>
2024-03-22 10:06:16 -0600

    remove db for pull request

--- lib/debug.py
+++ lib/debug.py
@@ -6,13 +6,5 @@ from classes.Visit import Visit
 Visit.drop_table()
 Visit.create_table()
 
-test = Visit(2, "sample description", "sample date", "sample user", "sample restaurant")
-test_2 = Visit(2, "sample description", "sample date", "sample user", "sample restaurant")
-test_3 = Visit(2, "sample description", "sample date", "sample user", "sample restaurant")
-Visit.create(2, "sample description", "sample date", "sample user", "sample restaurant")
 
-test.save()
-test_2.save()
-test_3.save()
-test_3.delete()
 

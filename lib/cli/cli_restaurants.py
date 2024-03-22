00d8df438080053a29705871526c9a477bd977ea
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-03-22 17:04:32 -0600

    Adds view_all functionality to Restaurants cli

--- lib/cli/cli_restaurants.py
+++ lib/cli/cli_restaurants.py
@@ -8,64 +8,12 @@ def restaurants():
     """Restaurants commands."""
     pass
 
-@restaurants.command()
-def view_all_restaurants():
-    """View all restaurants."""
-    def next_page():
-        click.echo('Next Page')
-
-    def prev_page():
-        click.echo('Previous Page')
-
-    all = Restaurant.get_all()
-
-    #limit search by 20
-    restaurants = all[0:20]
-
-    #print the name and cuisine 
-    for restaurant in restaurants:
-        click.echo(f"{restaurant.id}. {restaurant.name} || {restaurant.cuisine}")
-        
-    click.echo("\nRestaurants Menu:")
-    click.echo("1: Next Page")
-    click.echo("2: Previous Page")
-    click.echo("x: Back to Main Menu")
-
-    view_all_options = {
-        '1': next_page,
-        '2': prev_page
-    }
-
-    while True:
-        choice = click.prompt("Please enter your choice", type=str)
-
-        if choice in view_all_options:
-            click.clear()
-            view_all_options[choice]()  # Invoke the chosen function
-        elif choice == 'x':
-            click.clear()
-            break #exit the restaurants_menu
-        else:
-            click.echo("Invalid choice. Please try again.")
 
 
-@restaurants.command()
-def filter_by_cuisine():
-    """Filter restaurants by cuisine."""
-    pass
-
-@restaurants.command()
-def filter_by_location():
-    """Filter restaurants by location."""
-    click.echo("Restaurants filtered by location.")
-
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # => Restaurants Menu <=  # # # # # # # # # # # # # # #
 def restaurants_menu():
-    """Displays the restaurants menu and prompts for action."""
-    click.echo("\nRestaurants Menu:")
-    click.echo("1: View All")
-    click.echo("2: Filter by Cuisine")
-    click.echo("3: Filter by Location")
-    click.echo("x: Back to Main Menu")
+
 
     restaurants_options = {
         '1': view_all_restaurants,
@@ -74,6 +22,12 @@ def restaurants_menu():
     }
 
     while True:
+        """Displays the restaurants menu and prompts for action."""
+        click.echo("\nRestaurants Menu:")
+        click.echo("1: View All")
+        click.echo("2: Filter by Cuisine")
+        click.echo("3: Filter by Location")
+        click.echo("x: Back to Main Menu")
         choice = click.prompt("Please enter your choice", type=str)
 
         if choice in restaurants_options:
@@ -83,4 +37,70 @@ def restaurants_menu():
             click.clear()
             break #exit the restaurants_menu
         else:
+            click.clear()
             click.echo("Invalid choice. Please try again.")
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # => 1. View All Restaurants <=  # # # # # # # # # # # # # 
+def view_all_restaurants():
+    """View all restaurants."""
+
+    all = Restaurant.get_all()
+    page = [0,20]
+    restaurants = all[page[0]: page[1]]
+
+    def parse_by_page():
+        pass
+
+    def next_page():
+        p[0] += 20
+        p[1] += 20
+        restaurants = all[page[0]: page[1]]
+
+    def prev_page():
+        click.echo('Previous Page')
+
+
+    #limit search by 20
+
+    #print the name and cuisine 
+    for restaurant in restaurants:
+        click.echo(f"{restaurant.id}. {restaurant.name} || {restaurant.cuisine}")
+        
+
+    view_all_options = {
+        '1': next_page,
+        '2': prev_page
+    }
+
+    while True:
+        click.echo("\nRestaurants Menu:")
+        click.echo('#: View Restaurant by input')
+        click.echo("Next: Next Page")
+        click.echo("Prev: Previous Page")
+        click.echo("x: Back to Main Menu")
+        choice = click.prompt("Please enter your choice", type=str)
+
+        if choice in view_all_options:
+            click.clear()
+            view_all_options[choice]()  # Invoke the chosen function
+        elif choice == 'x':
+            click.clear()
+            break #exit the restaurants_menu
+        else:
+            click.clear()
+            click.echo("Invalid choice. Please try again.")
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # => 2. Filter By Cuisine <=  # # # # # # # # # # # # # 
+
+def filter_by_cuisine():
+    """Filter restaurants by cuisine."""
+    click.echo("Restaurants filtered by cuisine.")
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # => 3. Filter By Location <=  # # # # # # # # # # # # # 
+
+def filter_by_location():
+    """Filter restaurants by location."""
+    click.echo("Restaurants filtered by location.")

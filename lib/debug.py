848d08ba87bedc64ea88363c9604fe384ca787cf
pwangy <blipsandclicks@gmail.com>
2024-03-22 13:54:27 -0700

    troubleshooting with Matteo

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 # lib/debug.py
 
-from models.__init__ import CONN, CURSOR
+from classes.__init__ import CONN, CURSOR
 import ipdb
+from classes.post import Post
+from classes.task import Task
+from classes.reviewer import Reviewer
 
+Task.drop_table()
+Reviewer.drop_table()
+Post.drop_table()
 
+Reviewer.create_table()
+Post.create_table()
+Task.create_table()
+
+r = Reviewer.create('Jasmine Patel')
+arr = Reviewer('Bob Ross')
+arr.save()
+print('herro!')
 ipdb.set_trace()

fc404c1c9fa0ff512305b26d89438d8bf5eda250
Sung-Jin Ahn <sahn0307@gmail.com>
2024-03-22 11:20:41 -0600

    fix visit delete function, and User instance_from_db method

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 # lib/debug.py
 
+import ipdb
 from classes.Visit import Visit
 
 Visit.drop_table()
 Visit.create_table()
 
+test = Visit(2, "sample description", "sample date", "sample user", "sample restaurant")
+test_2 = Visit(2, "sample description", "sample date", "sample user", "sample restaurant")
+test_3 = Visit(2, "sample description", "sample date", "sample user", "sample restaurant")
+Visit.create(2, "sample description", "sample date", "sample user", "sample restaurant")
 
+test.save()
+test_2.save()
+test_3.save()
+test_3.delete()
 
+ipdb.set_trace()

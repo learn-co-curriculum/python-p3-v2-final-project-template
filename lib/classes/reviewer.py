7c59167fa5b2088779a26fd5008a89a4dd2ec2cb
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-03-24 11:11:01 -0700

    all methods in same format as post

--- lib/classes/reviewer.py
+++ lib/classes/reviewer.py
@@ -1,7 +1,7 @@
 from classes.__init__ import CURSOR, CONN
 from faker import Faker
 class Reviewer:
-
+    all = {}
 
     def __init__(self, name, id=None):
         self.id = id
@@ -27,42 +27,47 @@ class Reviewer:
     @classmethod
     def create_table(cls):
         try:
-            sql = """
-                CREATE TABLE IF NOT EXISTS reviewers (
-                id INTEGER PRIMARY KEY,
-                name TEXT
+            with CONN:
+                CURSOR.executescript(
+                    """
+                    BEGIN;
+                    CREATE TABLE IF NOT EXISTS reviewers (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT
+                    );
+                    CREATE INDEX idx_full_name ON reviewers (full_name);
+                    COMMIT
+                    """
                 )
-            """
-            CURSOR.execute(sql)
-            CONN.commit()
         except Exception as e:
-            CONN.rollback()
             return e
         
     @classmethod
     def drop_table(cls):
         try:
-            sql = """
-                DROP TABLE IF EXISTS reviewers;
-            """
-            CURSOR.execute(sql)
-            CONN.commit()
+            with CONN:
+                CURSOR.execute(
+                    """
+                    DROP TABLE IF EXISTS reviewers;
+                    """
+                )
         except Exception as e:
-            CONN.rollback()
             return e
     
     def save(self):
         try:
-            sql = """
-                INSERT INTO reviewers (name)
-                VALUES (?)
-            """
-            CURSOR.execute(sql, (self.name,))
-            CONN.commit()
-            self.id = CURSOR.lastrowid
-            return self
+            with CONN:
+                CURSOR.execute(
+                    """
+                    INSERT INTO reviewers (name)
+                    VALUES (?)
+                    """
+                )
+                CONN.commit()
+                self.id = CURSOR.lastrowid
+                type(self).all[self.id] = self
+                return self
         except Exception as e:
-            CONN.rollback()
             return e
 
     @classmethod
@@ -90,24 +95,49 @@ class Reviewer:
 
     def delete(self):
         try:
-            sql = """
-                DELETE FROM reviewers
-                WHERE id = ?
-            """
-            CURSOR.execute(sql, (self.id,))
+            with CONN:
+                CURSOR.execute(
+                    """
+                    DELETE FROM reviewers
+                    WHERE id = ?
+                    """,
+                    (self.id,),
+                )
             CONN.commit()
+            del type(self).all[self.id]
+            self.id = None
         except Exception as e:
-            CONN.rollback()
             return e
         
 
-
-    def find_by_id(self):
-        pass
+    @classmethod
+    def find_by_id(cls, id):
+        try:
+            CURSOR.execute(
+                f"""
+                SELECT * FROM reviewers
+                WHERE id is ?;
+            """,
+            (id,),
+            )
+            row = CURSOR.fetchone()
+            return cls(row[1]) if row else None
+        except Exception as e:
+            return e
 
     @classmethod
-    def get_all(self):
-        pass
+    def get_all(cls):
+        try:
+            with CONN:
+                CURSOR.execute(
+                    """
+                    SELECT * FROM reviewers;
+                    """
+                )
+                rows = CURSOR.fetchall()
+                return [cls(row[1]) for row in rows]
+        except Exception as e:
+            return e
 
     def tasks(self):
         from classes.task import Task

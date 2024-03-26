3719b9fec94031919dce6f1884035f6382021f7f
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-03-26 17:10:11 -0600

    Adds functionality to visits page

--- lib/cli/visits_page.py
+++ lib/cli/visits_page.py
@@ -10,11 +10,36 @@ def view_visit(visit_id):
     exit = False
     chosen_visit = Visit.get_visit_by_id(visit_id)
 
+    while not exit:
+        choice = click.prompt(f'\nEdit or Delete restaurant visit on {chosen_visit.date}? (e/d)').lower()
+        if choice == 'd':
+            delete_visit(chosen_visit)
+        elif choice == 'e':
+            edit_visit(chosen_visit)
+            break
+        else:
+            print('Please enter a valid option')
+
+def delete_visit(chosen_visit):
+    pass
+
+def edit_visit(chosen_visit):
+    pass
+
 
 
 visits_page = define_page("visits", "Visits")
 # Add options for visits page
-visits_page.add_option("Option 1", lambda: print("Visits - Option 1"))
-visits_page.add_option("Option 2", lambda: print("Visits - Option 2"))
+
+while current_user:
+    visits = User.current_user.visits() 
+
+print(visits)
+
+
+# for visit in visits:
+
+#     visits_page.add_option(f'{visit.date}', lambda: print("Visits - Option 1"))
+
 # ... (Add more options as needed)
 

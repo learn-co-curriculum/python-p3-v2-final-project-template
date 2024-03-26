4a7ff203fd327cb4cbf7248f3948fe51677bafb4
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-03-26 12:05:10 -0700

    reviewer helpers and cli with some post

--- lib/cli.py
+++ lib/cli.py
@@ -2,9 +2,17 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    list_reviewers,
+    find_reviewer_by_id,
+    create_reviewer,
+    update_reviewers,
+    delete_reviewer,
+    list_posts,
+    find_post_by_id,
+    create_post,
+    update_post,
+    delete_post
 )
-
 
 def main():
     while True:
@@ -13,16 +21,32 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            list_reviewers()
+        elif choice == "2":
+            find_reviewer_by_id()
+        elif choice == "3":
+            create_reviewer()
+        elif choice == "4":
+            update_reviewers()
+        elif choice == "5":
+            delete_reviewer()
+        elif choice == "6":
+            list_posts()
+        elif choice == "7":
+            find_post_by_id()
+        elif choice == "8":
+            create_post()
+        elif choice == "9":
+            update_post()
+        elif choice == "10":
+            delete_post()
         else:
             print("Invalid choice")
-
 
 def menu():
     print("Please select an option:")
     print("0. Exit the program")
     print("1. Some useful function")
-
 
 if __name__ == "__main__":
     main()
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,72 @@
 # lib/helpers.py
-
-def helper_1():
-    print("Performing useful function#1.")
-
+from classes.reviewer import Reviewer
+from classes.post import Post
 
 def exit_program():
     print("Goodbye!")
     exit()
+
+#Reviewer
+def list_reviewers():
+    reviewers = Reviewer.get_all()
+    for reviewer in reviewers:
+        return reviewer
+    
+def find_reviewer_by_id():
+    id_ = ("Enter the reviewer's id: ")
+    reviewer = Reviewer.find_by_id(id_)
+    print(reviewer) if reviewer else print(f'Reviewer {id_} not found')
+
+def create_reviewer():
+    id = input("Enter the reviewer's id: ")
+    name = input("Enter the reviewer's name: ")
+    try:
+        reviewer = Reviewer.create(id, name)
+        print(f'Success: {reviewer}')
+    except Exception as e:
+        return e
+
+def update_reviewers():
+    id_ = input("Enter the reviewer's id: ")
+    if reviewer := Reviewer.find_by_id(id_):
+        try:
+            name = input("Enter the reviewer's new name: ")
+            reviewer.name = name
+
+            reviewer.update()
+            print(f'Success: {reviewer}')
+        except Exception as e:
+            return e
+    else:
+        print(f'Reviewer {id_} not found')
+
+def delete_reviewer():
+    id_ = input("Enter the reviewer's id: ")
+    if reviewer := Reviewer.find_by_id(id_):
+        reviewer.delete()
+        print(f'Reviewer {id_} deleted')
+    else:
+        print(f'Reviewer {id_} not found')
+
+#Post
+def list_posts():
+    posts = Post.get_all()
+    for post in posts:
+        return post
+
+def find_post_by_id():
+    id_ = input("Enter the post's id: ")
+    post = Post.find_by_id(id_)
+    print(post) if post else print(f'Post {id_} not found')
+
+def create_post():
+    pass
+
+def update_post():
+    pass
+
+def delete_post():
+    pass
+#Task
+def list_tasks():
+    pass

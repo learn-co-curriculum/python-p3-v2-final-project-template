370bd859dc221c8ecb05aa4a01132f6b4d5362ff
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-03-26 16:50:20 -0600

    Visit page adjustments

--- lib/cli/visits_page.py
+++ lib/cli/visits_page.py
@@ -1,4 +1,16 @@
+import click
+from rich import print
+from classes.User import User
+from classes.Visit import Visit
 from cli.pages import define_page
+
+
+def view_visit(visit_id):
+    global exit
+    exit = False
+    chosen_visit = Visit.get_visit_by_id(visit_id)
+
+
 
 visits_page = define_page("visits", "Visits")
 # Add options for visits page
